3f469fca2252d04abbe26d31bf93c07503df15f8
Allen Bonifacio <aabonifacio@myseneca.ca>
2026-03-11 19:39:09 -0400

    lab5a complete

---
+++ lab5a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Author ID: Allen052897
+
+def read_file_string(file_name):
+    f = open(file_name, 'r')
+    read_data = f.read()
+    f.close()
+    return read_data
+
+
+def read_file_list(file_name):
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    f.close()
+
+    list_of_lines = []
+    for line in lines:
+        list_of_lines.append(line.strip('\n'))
+
+    return list_of_lines
+
+
+if __name__ == "__main__":
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
